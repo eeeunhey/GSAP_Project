a793736032bb3e05187397eab6133369d953c7ee
eunhye <dmsgp2627@naver.com>
2025-11-25 15:09:03 +0900

    홈페이지 수정

--- GSAP_React/src/pages/HomePage.jsx
+++ GSAP_React/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div>
         <div>
             <h1>
-                GSAP TO 학습 페이지 초기 작업
+                GSAP TO 학습 페이지 초기 작업dㅇㅇ
                 
             </h1>
             <Link to="/gaspTo">Gsap To 페이지로 이동</Link>
